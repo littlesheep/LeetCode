1d7a137bb911b763363eef9a5361b8cbff7d1f81
Yang Chen <chenyangls@gmail.com>
2016-11-14 14:00:41 -0800

    second commit

--- ReconstructItinerary.java
+++ ReconstructItinerary.java
@@ -3,8 +3,7 @@ import java.util.*;
 public class ReconstructItinerary {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[][] tickets = {{"EZE","TIA"},{"EZE","HBA"},{"AXA","TIA"},{"JFK","AXA"},{"ANU","JFK"},{"ADL","ANU"},{"TIA","AUA"},{"ANU","AUA"},{"ADL","EZE"},{"ADL","EZE"},{"EZE","ADL"},{"AXA","EZE"},{"AUA","AXA"},{"JFK","AXA"},{"AXA","AUA"},{"AUA","ADL"},{"ANU","EZE"},{"TIA","ADL"},{"EZE","ANU"},{"AUA","ANU"}};
-		
+		String[][] tickets = {{"JFK","ATL"},{"JFK","SFO"},{"SFO","JFK"}};
 		ReconstructItinerary r = new ReconstructItinerary();
 		List<String> list = r.findItinerary(tickets);
 		for (String str : list) {
@@ -12,59 +11,38 @@ public class ReconstructItinerary {
 		}
 
 	}
-    public List<String> findItinerary(String[][] tickets) {
-        Map<String, List<String>> map = new HashMap<String, List<String>>();
+	
+	LinkedList<String> list = new LinkedList<String>();
+	
+	public List<String> findItinerary(String[][] tickets) {
+        Map<String, PriorityQueue<String>> map = new HashMap<String, PriorityQueue<String>>();
         readTicket(tickets, map);
 
-        String start = "JFK";
-        List<String> list = new ArrayList<String>();
-        list.add(start);
-        helper(map, list, start);
+        helper(map, "JFK");
         return list;
     }
     
-    private boolean helper(Map<String, List<String>> map, List<String> list, String start) {
-        if (map.isEmpty()) return true;
-        if (!map.containsKey(start)) return false;
- 
-        List<String> dests = map.get(start);
-        //if (dests.isEmpty()) return false;
-        for (String next : dests) {
-           // if (map.containsKey(next)) {
-                list.add(next);
-                List<String> tmp = new ArrayList<String>(dests);
-                tmp.remove(next);
-                if (tmp.isEmpty()) map.remove(start);
-                else map.put(start, tmp);
-                if (helper(map, list, next)) break;
-                list.remove(list.size()-1);
-                map.put(start, dests);
-           // }
+    private void helper(Map<String, PriorityQueue<String>> map, String start) {
+        while (map.containsKey(start) && !map.get(start).isEmpty()) {
+        	String next = map.get(start).poll();
+            helper(map, next);
         }
-        if (map.isEmpty()) return true;
-        else return false;
+        list.addFirst(start);
         //return true;
     }
     
-    private void readTicket(String[][] tickets, Map<String, List<String>> map){
-        for (int i = 0; i < tickets.length; i++) {
-            String[] pair = tickets[i];
-            String from = pair[0];
-            String to = pair[1];
-            if (map.containsKey(from)) {
-                map.get(from).add(to);
+    private void readTicket(String[][] tickets, Map<String, PriorityQueue<String>> map){
+        for (String[] ticket : tickets) {
+
+            if (map.containsKey(ticket[0])) {
+                map.get(ticket[0]).add(ticket[1]);
             } else {
-                List<String> val = new ArrayList<String>();
-                val.add(to);
-                map.put(from, val);
+                PriorityQueue<String> val = new PriorityQueue<String>();
+                val.add(ticket[1]);
+                map.put(ticket[0], val);
             }
         }
-        
-        for (String key : map.keySet()) {
-            List<String> val = map.get(key);
-            Collections.sort(val);
-            map.put(key, val);
-        }
+
     }
     
 
